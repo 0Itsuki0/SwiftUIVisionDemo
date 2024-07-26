80e3fb906da6f7c5197facf9bbb478505d56b130
itsuki <itsuki.enjyoy@gmail.com>
2024-07-26 10:00:32 +0900

    Initial Commit

---
+++ VisionKitDemo/VisionKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionKitDemoApp.swift
+//  VisionKitDemo
+//
+//  Created by Itsuki on 2024/07/26.
+//
+
+import SwiftUI
+
+@main
+struct VisionKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
